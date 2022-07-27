0d55baf0bff16225bd86cf5c70500255b1bd7f7d
Diemas <info@diemas.dev>
2022-07-27 15:26:58 +0200

    feat: 35 mixing html and webgl

--- 35-mixing-html-and-webgl/src/script.js
+++ 35-mixing-html-and-webgl/src/script.js
@@ -3,27 +3,37 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { gsap } from 'gsap'
+
 /**
  * Loaders
  */
+let sceneReady = false
 const loadingBarElement = document.querySelector('.loading-bar')
-
 const loadingManager = new THREE.LoadingManager(
   // Loaded
   () => {
-    gsap.delayedCall(0.5, () => {
+    // Wait a little
+    window.setTimeout(() => {
+      // Animate overlay
       gsap.to(overlayMaterial.uniforms.uAlpha, {
         duration: 3,
         value: 0,
         delay: 1,
       })
 
+      // Update loadingBarElement
       loadingBarElement.classList.add('ended')
       loadingBarElement.style.transform = ''
-    })
+    }, 500)
+
+    window.setTimeout(() => {
+      sceneReady = true
+    }, 2000)
   },
+
   // Progress
-  (_, itemsLoaded, itemsTotal) => {
+  (itemUrl, itemsLoaded, itemsTotal) => {
+    // Calculate the progress and update the loadingBarElement
     const progressRatio = itemsLoaded / itemsTotal
     loadingBarElement.style.transform = `scaleX(${progressRatio})`
   },
@@ -43,9 +53,12 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-// Overlay
-const overlayGeometry = new THREE.PlaneBufferGeometry(2, 2, 1, 1)
+/**
+ * Overlay
+ */
+const overlayGeometry = new THREE.PlaneGeometry(2, 2, 1, 1)
 const overlayMaterial = new THREE.ShaderMaterial({
+  // wireframe: true,
   transparent: true,
   uniforms: {
     uAlpha: { value: 1 },
@@ -108,14 +121,30 @@ debugObject.envMapIntensity = 2.5
 /**
  * Models
  */
-gltfLoader.load('/models/FlightHelmet/glTF/FlightHelmet.gltf', gltf => {
-  gltf.scene.scale.set(10, 10, 10)
-  gltf.scene.position.set(0, -4, 0)
+gltfLoader.load('/models/DamagedHelmet/glTF/DamagedHelmet.gltf', gltf => {
+  gltf.scene.scale.set(2.5, 2.5, 2.5)
   gltf.scene.rotation.y = Math.PI * 0.5
   scene.add(gltf.scene)
 
   updateAllMaterials()
 })
+
+// Points
+const raycaster = new THREE.Raycaster()
+const points = [
+  {
+    position: new THREE.Vector3(1.55, 0.3, -0.6),
+    element: document.querySelector('.point-0'),
+  },
+  {
+    position: new THREE.Vector3(0.5, 0.8, -1.6),
+    element: document.querySelector('.point-1'),
+  },
+  {
+    position: new THREE.Vector3(1.6, -1.3, -0.7),
+    element: document.querySelector('.point-2'),
+  },
+]
 
 /**
  * Lights
@@ -189,6 +218,33 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 const tick = () => {
   // Update controls
   controls.update()
+
+  if (sceneReady) {
+    for (const point of points) {
+      const screenPosition = point.position.clone()
+      screenPosition.project(camera)
+
+      raycaster.setFromCamera(screenPosition, camera)
+      const intersects = raycaster.intersectObjects(scene.children, true)
+
+      if (intersects.length === 0) {
+        point.element.classList.add('visible')
+      } else {
+        const intersectionDistance = intersects[0].distance
+        const pointDistance = point.position.distanceTo(camera.position)
+
+        if (intersectionDistance < pointDistance) {
+          point.element.classList.remove('visible')
+        } else {
+          point.element.classList.add('visible')
+        }
+      }
+
+      const translateX = screenPosition.x * sizes.width * 0.5
+      const translateY = -screenPosition.y * sizes.height * 0.5
+      point.element.style.transform = `translate(${translateX}px, ${translateY}px)`
+    }
+  }
 
   // Render
   renderer.render(scene, camera)
